f970ed6b916b1f7b2974df19d8444bd8040ace59
MichalJedinak <109184193+MichalJedinak@users.noreply.github.com>
2023-11-09 23:15:20 +0100

    web table connect

--- src/web/js/srcript.js
+++ src/web/js/srcript.js
@@ -1,26 +1,26 @@
-// var mainContent = document.getElementById('content');
-
-//         fetch('#add') 
-//             .then(function(response) {
-//                 return response.text();
-//             })
-//             .then(function(data) {
-//                 mainContent.innerHTML = data;
-//             })
-//             .catch(function(error) {
-//                 console.error('Nepodarilo sa načítať obsah', error);
-//             });
 var section  = document.getElementById("section");
         add  = document.getElementById("add");
        edit  = document.getElementById("edit");
        section.style.visibility=hidden;
     function ShowAdd() {
-        section.style.visibility="visible";
-        add.style.opacity=1;           
-        edit.style.opacity=0;     
+        section.style.visibility = "visible";
+        add.style.opacity = 1;
+        edit.style.opacity = 0;
+        add.style.visibility = "visible";
+        edit.style.visibility = "hidden";
     }
     function ShowEdit() {
-        section.style.visibility="visible";
-        add.style.opacity=0;           
-        edit.style.opacity=1;       
+        section.style.visibility = "visible";
+        add.style.opacity = 0;
+        edit.style.opacity = 1;
+        add.style.visibility = "hidden";
+        edit.style.visibility = "visible";
     }
+
+    
+ 
+ 
+    
+   
+
+
